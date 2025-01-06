f4e301db040b4cc1a58e73e532b3c871da8b573b
tatarinov <tatarinov@protei-lab.ru>
2025-01-06 16:27:01 +0300

    bi

---
+++ client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	api "chat/api/grpc"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"io"
+	"log"
+	"math/rand"
+)
+
+func main() {
+	host := "localhost"
+	port := "8080"
+	fieldSize := int32(rand.Intn(6))
+	conn, err := grpc.NewClient(fmt.Sprintf("%s:%s", host, port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	client := api.NewBattleshipServiceClient(conn)
+	stream, err := client.Game(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = stream.Send(&api.Request{Data: &api.Request_Start{Start: &api.StartGame{FieldSize: fieldSize}}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := int32(0); i < fieldSize; i++ {
+		for j := int32(0); j < fieldSize; i++ {
+			err = stream.Send(&api.Request{Data: &api.Request_Coordinate{Coordinate: &api.AttackCoordinate{X: i, Y: j}}})
+			if err != nil {
+				log.Fatal(err)
+			}
+			resp, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("End of stream")
+					break
+				}
+				log.Fatal(err)
+			}
+			log.Println(resp.Status)
+		}
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	api "chat/api/grpc"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"io"
+	"math/rand"
+	"net"
+)
+
+type Server struct {
+	api.UnimplementedBattleshipServiceServer
+}
+
+func (s *Server) Game(stream api.BattleshipService_GameServer) error {
+	msg, err := stream.Recv()
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to receive: %v", err))
+		return err
+	}
+	startGame := msg.GetStart()
+	if startGame == nil {
+		logger.Error(fmt.Sprintf("Failed to receive startGame: %v", err))
+		return status.Errorf(codes.InvalidArgument, "You must provide a startGame")
+	}
+	battleField := make([][]int, startGame.FieldSize)
+	for i := range battleField {
+		battleField[i] = make([]int, startGame.FieldSize)
+	}
+	battleField[rand.Intn(int(startGame.FieldSize))][rand.Intn(int(startGame.FieldSize))] = 1
+	fmt.Println(battleField)
+
+outerLoop: // Метка для внешнего цикла
+	for {
+		msgPack, errStream := stream.Recv()
+		if errStream != nil {
+			if errStream == io.EOF {
+				logger.Error("Stream closed")
+				break // Завершение внутреннего цикла
+			}
+			logger.Error(errStream.Error())
+			break // Завершение внутреннего цикла
+		}
+		logger.Info(fmt.Sprintf("Received: %v", msgPack))
+		cord := msgPack.GetCoordinate()
+		if cord == nil {
+			continue
+		}
+		switch battleField[cord.X][cord.Y] {
+		case 1:
+			err = stream.Send(&api.AttackInformation{Status: "You win"})
+			if err != nil {
+				return err
+			}
+			break outerLoop // Завершение внешнего цикла
+		case 0:
+			battleField[cord.X][cord.Y] = 2
+			err = stream.Send(&api.AttackInformation{Status: "You missed"})
+			if err != nil {
+				return err
+			}
+		case 2:
+			err = stream.Send(&api.AttackInformation{Status: "You have already attacked this coordinate"})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func StartGrpcServer() error {
+	lis, err := net.Listen("tcp",
+		fmt.Sprintf("%s:%d", "localhost", 8080))
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to listen: %v", err))
+		return err
+	}
+	s := grpc.NewServer()
+	api.RegisterBattleshipServiceServer(s, &Server{})
+	logger.Info(fmt.Sprintf("Starting server on port %d", 8080))
+	if err = s.Serve(lis); err != nil {
+		logger.Error(fmt.Sprintf("Failed to start server: %v", err))
+		return err
+	}
+	return nil
+}
